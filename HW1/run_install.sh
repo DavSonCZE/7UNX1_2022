d751b7af4cff2d6f220ff2e0deff00a4c260228c
DavSonCZE <40635601+DavSonCZE@users.noreply.github.com>
2022-11-11 17:06:08 +0100

    Update run_install.sh

--- HW1/run_install.sh
+++ HW1/run_install.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
+user=${whoami}
+
 echo "#Downloading file: davsonFIleRenamer_0.0.1_arm64.zip from github"
-wget --user=renamer --ask-password https://github.com/DavSonCZE/7UNX1_2022/blob/main/HW1/davsonfilenamer_master.tar -P /home/renamer/Downloads
+wget 'https://raw.github.com/DavSonCZE/7UNX1_2022/blob/main/HW1/davsonfilenamer_master.tar' -P /home/${user}/Downloads
 echo "#Unpacking file: davsonFIleRenamer_0.0.1_arm64.tar"
-tar -zxf /home/renamer/Downloads/davsonfilenamer_master.tar --directory /home/renamer/Downloads
+tar -zxf /home/${user}/Downloads/davsonfilenamer_master.tar --directory /home/${user}/Downloads
 
 echo "#Creating directory dir for script:"
-mkdir /home/renamer/Desktop/davsonrenamer_Control
-chmod +x /home/renamer/Downloads/davsonfilenamer_master/install.sh
-chmod +x /home/renamer/Downloads/davsonfilenamer_master/init_defaultfolder.sh
-chmod +x /home/renamer/Downloads/davsonfilenamer_master/controller.sh
-chmod +x /home/renamer/Downloads/davsonfilenamer_master/uninstall.sh
+mkdir /home/${user}/Desktop/davsonrenamer_Control
+chmod +x /home/${user}/Downloads/davsonfilenamer_master/install.sh
+chmod +x /home/${user}/Downloads/davsonfilenamer_master/init_defaultfolder.sh
+chmod +x /home/${user}/Downloads/davsonfilenamer_master/controller.sh
+chmod +x /home/${user}/Downloads/davsonfilenamer_master/uninstall.sh
 echo "#Copy important files"
-cp /home/renamer/Downloads/davsonfilenamer_master/install.sh /home/renamer/Desktop/davsonrenamer_Control/install.sh
-cp /home/renamer/Downloads/davsonfilenamer_master/init_defaultfolder.sh /home/renamer/Desktop/davsonrenamer_Control/init_defaultfolder.sh
-cp /home/renamer/Downloads/davsonfilenamer_master/controller.sh /home/renamer/Desktop/davsonrenamer_Control/controller.sh
-cp /home/renamer/Downloads/davsonfilenamer_master/install.sh /home/renamer/Desktop/davsonrenamer_Control/uninstall.sh
+cp /home/${user}/Downloads/davsonfilenamer_master/install.sh /home/${user}/Desktop/davsonrenamer_Control/install.sh
+cp /home/${user}/Downloads/davsonfilenamer_master/init_defaultfolder.sh /home/${user}/Desktop/davsonrenamer_Control/init_defaultfolder.sh
+cp /home/${user}/Downloads/davsonfilenamer_master/controller.sh /home/${user}/Desktop/davsonrenamer_Control/controller.sh
+cp /home/${user}/Downloads/davsonfilenamer_master/install.sh /home/${user}/Desktop/davsonrenamer_Control/uninstall.sh
